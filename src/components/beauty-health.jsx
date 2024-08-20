dbe27b04721a95420026198cfee54f78fcb90872
Muradjonov <f93230042@gmail.com>
2024-08-20 15:16:46 +0500

    done

---
+++ src/components/beauty-health.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Beautyhealth = () => {
+  return (
+    <div>Beautyhealth</div>
+  )
+}
+
+export default Beautyhealth
---
+++ src/components/home-tech.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Hometech = () => {
+  return (
+    <div>Hometech</div>
+  )
+}
+
+export default Hometech

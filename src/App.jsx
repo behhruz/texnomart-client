7316087c7efcb7a760ca4a9a1f9e1a691e0ccd47
behruz20 <itfrontuchun@gmail.com>
2024-09-04 15:12:43 +0500

    done texnomart

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import Banner from './components/Banner';
 import About from './components/About';
-import Cattegory from './components/Swipper';
-import Footer from './components/footer';
 import Header from './components/Header';
 import FooterSection from './components/FooterSection';
+import Cattegory from './components/Swipper';
+import Footer from './components/Footer';
 
 
 
